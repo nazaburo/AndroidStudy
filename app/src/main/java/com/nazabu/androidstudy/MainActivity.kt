91da391d55e60187a8a82a98354792d9cb045fba
nazaburo <84699252+nazaburo@users.noreply.github.com>
2021-06-06 16:51:19 +0900

    Initial commit

---
+++ app/src/main/java/com/nazabu/androidstudy/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nazabu.androidstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nazabu.androidstudy.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
