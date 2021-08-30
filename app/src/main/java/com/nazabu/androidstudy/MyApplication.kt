8735665da1f42b13ec8be108de1883862f051138
nazaburo <84699252+nazaburo@users.noreply.github.com>
2021-08-31 00:02:24 +0900

    add timber

--- app/src/main/java/com/nazabu/androidstudy/MyApplication.kt
+++ app/src/main/java/com/nazabu/androidstudy/MyApplication.kt
@@ -2,6 +2,19 @@ package com.nazabu.androidstudy
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
