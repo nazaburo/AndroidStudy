a976b6ee977dd78232d3f7d6ca5341fa5dc6570f
nazaburo <84699252+nazaburo@users.noreply.github.com>
2021-12-26 23:41:07 +0900

    Add Databind

--- app/src/main/java/com/nazabu/androidstudy/ui/XmlActivity.kt
+++ app/src/main/java/com/nazabu/androidstudy/ui/XmlActivity.kt
@@ -2,12 +2,22 @@ package com.nazabu.androidstudy.ui
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.nazabu.androidstudy.R
+import com.nazabu.androidstudy.databinding.ActivityXmlBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class XmlActivity : AppCompatActivity(R.layout.activity_xml) {
+class XmlActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityXmlBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityXmlBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
 
     companion object {
         fun createIntent(context: Context): Intent = Intent(context, XmlActivity::class.java)
